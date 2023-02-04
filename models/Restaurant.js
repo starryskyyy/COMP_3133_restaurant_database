0dd70d5d840e1c3f95349223b7c3d61e31b8bcb8
Elizaveta Vygovskaia <vygovskaia.e@gmail.com>
2023-02-04 13:06:02 -0500

    created db schema

---
+++ models/Restaurant.js
@@ -0,0 +1,58 @@
+const mongoose = require('mongoose')
+
+const RestaurantSchema = new mongoose.Schema({
+    address: {
+        building: {
+            type: String,
+            trim: true
+        },
+        street: {
+            type: String,
+            required: [true, "Please provide street name"],
+            trim: true
+        },
+        zipcode: {
+            type: String,
+            required: [true, "Please provide zipcode"],
+            trim: true
+        }
+    },
+    city: {
+        type: String,
+        required: [true, "Please provide city"],
+        trim: true
+    },
+    cuisine: {
+        type: String,
+        required: [true, "Please provide cousine"],
+        trim: true
+    },
+    name: {
+        type: String,
+        required: [true, "Please provide restaurant name"],
+        trim: true
+    },
+    created: {
+        type: Date,
+        default: Date.now
+    },
+    updatedat: {
+        type: Date,
+        default: Date.now
+    },
+})
+
+RestaurantSchema.pre('save', (next) => {
+    console.log("Before Save")
+    let now = Date.now()
+
+    this.updatedat = now
+
+    if (!this.created) {
+        this.created = now
+    }
+    next()
+});
+
+const Restaurant = mongoose.model("Restaurant", RestaurantSchema);
+module.exports = Restaurant;
