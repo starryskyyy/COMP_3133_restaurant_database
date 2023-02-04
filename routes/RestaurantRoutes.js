3ee28e014c68632405769d660c30660b2d3c43df
Elizaveta Vygovskaia <vygovskaia.e@gmail.com>
2023-02-04 13:54:04 -0500

    added sort by

--- routes/RestaurantRoutes.js
+++ routes/RestaurantRoutes.js
@@ -16,19 +16,33 @@ app.get('/restaurants', async (req, res) => {
 // return all restaurant details by cuisine
 app.get('/restaurants/cuisine/:name', async (req, res) => {
     const name = req.params.name
-    const restaurants = await restaurantModel.find({cuisine : name})
-    
-    try {
-      if(restaurants.length != 0){
-        res.send(restaurants);
-      }else{
-        res.send(JSON.stringify({status:false, message: "No data found"}))
-      }
-    } catch (err) {
-      res.status(500).send(err);
-    }
-  });
+    const restaurants = await restaurantModel.find({ cuisine: name })
 
+    try {
+        if (restaurants.length != 0) {
+            res.send(restaurants);
+        } else {
+            res.send(JSON.stringify({ status: false, message: "No data found" }))
+        }
+    } catch (err) {
+        res.status(500).send(err);
+    }
+});
+
+// return: 
+//The selected columns must include id, cuisines, name, city, resturant_id
+//The sorting by the restaurant_id in Ascending or Descending Order based on parameter passed.
+
+app.get('/restaurants', async (req, res) => {
+
+    const restaurants = await restaurantModel.getRestaurants(req.query.sortBy)
+
+    try {
+        res.status(200).send(restaurants);
+    } catch (err) {
+        res.status(500).send(err);
+    }
+});
 /*  used create new restaurant to see data
 app.post('/restaurant', async (req, res) => {
     const restaurant = new restaurantModel(req.body);
